7ae0d6e9a1bc8b77e931845f6413e36324ea4a0d
Ares Fernández Ramos <aresis_7@hotmail.com>
2019-09-30 12:55:22 +0200

    comments

--- src/Seek.cpp
+++ src/Seek.cpp
@@ -13,5 +13,6 @@ void Seek::applySteeringForce(Agent *agent, float dtime)
 	Vector2D steering = agent->getTarget() - agent->getPosition();
 	steering.Normalize();
 	agent->setVelocity (steering * agent->getMaxVelocity());
-	agent->setPosition (agent->getPosition() + agent->getVelocity() * dtime);
+	agent->setPosition (agent->getPosition() + agent->getVelocity() * dtime); 
+	// This is NOT the Seek steering behavior! We must take into count acceleration!
 }
